542da65157f1c3859e3d0b6831aea53d2bc8012e
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-06-23 12:02:09 -0700

    Initial commit

---
+++ RCPL.hs
@@ -0,0 +1,50 @@
+module RCPL (
+    readLoop,
+    printLoop
+    ) where
+
+import Control.Monad (when)
+import Data.Text (Text, pack, unpack)
+import Data.Char (isSpace)
+import Pipes (Producer, Consumer, request, respond, forever, lift)
+import System.Console.Readline (
+    readline, addHistory, forcedUpdateDisplay, getLineBuffer )
+
+-- TODO: Throttle input
+
+prompt :: String
+prompt = "> "
+
+deleteInput :: String -> IO ()
+deleteInput string = putStr (go (length string))
+  where
+    go n | n <=  0   = ""
+         | n <  80   = replicate n '\BS' ++ "\ESC[K"
+         | otherwise = "\ESC[A\ESC[K" ++ go (n - 80)
+
+-- | 'readLoop' reads in values from the console using a @readline@-based prompt
+readLoop :: () -> Producer Text IO ()
+readLoop () = loop
+  where
+    loop = do
+        mLine <- lift $ readline prompt
+        case mLine of
+            Nothing     -> return ()
+            Just string -> do
+                lift $ when (not (all isSpace string)) $ addHistory string
+                respond (pack string)
+                loop
+{-# INLINABLE readLoop #-}
+
+{-| 'printLoop' prints all values to the console without clobbering the input
+    line
+-}
+printLoop :: () -> Consumer Text IO r
+printLoop () = forever $ do
+    text <- request ()
+    lift $ do
+        buffer <- getLineBuffer
+        deleteInput (prompt ++ buffer)
+        putStrLn (unpack text)
+        forcedUpdateDisplay
+{-# INLINABLE printLoop #-}
